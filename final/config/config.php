86c24648772beed14b9f021422284d9892b5ab21
Upendra237 <shahiupendra237@gmail.com>
2025-01-29 15:55:19 +0545

    twiking-for final

--- final/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Root path of the application (physical path on server)
-define('ROOT_PATH', dirname(dirname(__FILE__))); 
-
-// Root URL (web accessible path)
-define('ROOT_URL', '/'); // the root url for maindomain
-// define('ROOT_URL', '/repo_management_system'); //for subdirectory
-?>
--- final/config/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php'; //instead of calling config.php in all files
-require_once ROOT_PATH . '/includes/helpers/channel-helpers.php';
-?>
